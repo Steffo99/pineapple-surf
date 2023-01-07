5f729e6e15d3c31906103c37b5dea880366a2495
Stefano Pigozzi <me@steffo.eu>
2023-01-07 23:56:57 +0100

    Create growing crop tile

---
+++ island/CropTile.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+
+signal on_planted()
+signal on_complete()
+
+
+@onready var growth_timer: Timer = $GrowthTimer
+@onready var sprout_mesh: MeshInstance3D = $Plant/Sprout
+
+
+func plant():
+	growth_timer.start()
+	emit_signal("on_planted")
+
+
+func complete():
+	growth_timer.stop()
+	emit_signal("on_complete")
+
+
+func _process(_delta):
+	var scale_factor = 1 - (growth_timer.time_left / growth_timer.wait_time)
+	sprout_mesh.scale = Vector3(scale_factor, scale_factor, scale_factor)
