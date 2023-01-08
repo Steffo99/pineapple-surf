c9d4df4a86fd77520ab95fa92c1bf1faad5d4de0
Stefano Pigozzi <me@steffo.eu>
2023-01-08 10:11:38 +0100

    `MoveTowardsPlayer`: Use a target instead of the direct parent

--- island/MoveTowardsPlayer.gd
+++ island/MoveTowardsPlayer.gd
@@ -1,9 +1,9 @@
 extends Node
 
-@export var force_multiplier: float = 0.0
+@export var force_multiplier: float
+@export var target: RigidBody3D
 
 # Must be BELOW the player node to work!
-@onready var parent: RigidBody3D = get_parent()
 @onready var magnet_area: Area3D = $MagnetArea
 @onready var player: CharacterBody3D = get_tree().root.find_child("Player", true, false)
 
@@ -17,9 +17,9 @@ func capture():
 
 func _physics_process(delta):
 	if captured:
-		var direction = player.position - parent.position
+		var direction = player.position - target.position
 		var force = direction.normalized() * force_multiplier * delta
-		parent.apply_force(force)
+		target.apply_force(force)
 
 
 func _on_magnet_area_body_entered(body: Node3D):
