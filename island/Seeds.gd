2be0b42c14471a81836e07ed09bb069101b223b5
Stefano Pigozzi <me@steffo.eu>
2023-01-09 00:35:59 +0100

    Move player reference to @onready variable

--- island/Seeds.gd
+++ island/Seeds.gd
@@ -3,7 +3,9 @@ extends RigidBody3D
 
 @export var value = 5
 
+@onready var player := Singletons.player
+
 
 func _on_collectible_by_player_collected():
-	var ps: PeaShooter = Singletons.player.get_node("Head/OnHand/PeaShooter")
+	var ps: PeaShooter = player.get_node("Head/OnHand/PeaShooter")
 	ps.remaining += value
