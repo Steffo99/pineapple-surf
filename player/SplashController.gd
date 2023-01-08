56b59f42d3b730ef37311bd5c9b9c8d3aea24ff8
Stefano Pigozzi <me@steffo.eu>
2023-01-08 21:46:19 +0100

    Refactor SplashController

--- player/SplashController.gd
+++ player/SplashController.gd
@@ -1,13 +1,24 @@
 extends Node
 
 
-@onready var player: Node3D = $".."
-@onready var spawn_point: Vector3 = player.position
+@export_node_path(CharacterBody3D) var target_path: NodePath
+@export var splash_threshold: float = 0.0
+
+@onready var target: CharacterBody3D = get_node(target_path)
+@onready var spawn_point: Vector3 = target.position
 @onready var splash_sound: AudioStreamPlayer = $SplashSound
 
 
+signal splashed()
+
+
+func splash():
+	splash_sound.play()
+	target.position = spawn_point
+	target.velocity = Vector3.ZERO
+	emit_signal("splashed")
+
+
 func _process(_delta):
-	if player.position.y < 0:
-		splash_sound.play()
-		player.position = spawn_point
-		player.velocity = Vector3.ZERO
+	if target.position.y < splash_threshold:
+		splash()
