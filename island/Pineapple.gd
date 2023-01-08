d8504d39cc84e294d786911cd5a8b742391f880f
Stefano Pigozzi <me@steffo.eu>
2023-01-09 00:36:53 +0100

    Plant the seeds (he he) for pineapple behavior

--- island/Pineapple.gd
+++ island/Pineapple.gd
@@ -1,1 +1,10 @@
 extends RigidBody3D
+
+
+@onready var player := Singletons.player
+
+
+func _on_collectible_by_player_collected():
+	# var player: Node3D = Singletons.player
+	# player.scale *= 1.05
+	pass
