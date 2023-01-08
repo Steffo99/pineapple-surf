69384f9555563a7df1b0653990b7951b3f99379a
Stefano Pigozzi <me@steffo.eu>
2023-01-08 10:20:01 +0100

    Fix pineapple magnet

--- island/Producer.gd
+++ island/Producer.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 @export var produce_scene: PackedScene = preload("res://island/Pineapple.tscn")
 
+@onready var produce_container: Node3D = get_tree().root.find_child("Produce", true, false)
 var rng: RandomNumberGenerator
 
 
@@ -14,5 +15,6 @@ func _ready():
 func produce_with_random_force():
 	# TODO: Not sure it's a good idea attaching produce to the crop
 	var produce: RigidBody3D = produce_scene.instantiate()
-	add_child(produce)
+	produce.position = global_position
+	produce_container.add_child(produce)
 	produce.apply_impulse(Vector3.UP * rng.randf_range(15.0, 25.0))
