fb54a67393a2f1f6336994a272f074ddc81ffea3
Ichicoro <me@marte.dev>
2023-01-08 13:47:44 +0100

    Use singleton for Player access and despawn pineapple on Player collision

---
+++ Singletons.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var player: Player
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
--- island/MoveTowardsPlayer.gd
+++ island/MoveTowardsPlayer.gd
@@ -2,11 +2,12 @@ extends Node
 
 @export var force_multiplier: float
 @export var target: RigidBody3D
+@export var capture_radius: float = 1.5
 
-# Must be BELOW the player node to work!
 @onready var magnet_area: Area3D = $MagnetArea
-@onready var player: CharacterBody3D = get_tree().root.find_child("Player", true, false)
-
+@onready var capture_area: Area3D = $CaptureArea
+@onready var pop: AudioStreamPlayer3D = $"../Pop"
+@onready var mesh: MeshInstance3D = $"../PineappleMesh"
 
 var captured = false
 
@@ -17,12 +18,19 @@ func capture():
 
 func _physics_process(delta):
 	if captured:
-		var direction = player.position - target.position
+		var direction = Singletons.player.position - target.position
 		var force = direction.normalized() * force_multiplier * delta
 		target.apply_force(force)
 
 
 func _on_magnet_area_body_entered(body: Node3D):
-	if body.name == "Player":
+	if body is Player:
 		print("Player captured pickup!")
 		capture()
+
+
+func _on_capture_area_body_entered(body: Node3D) -> void:
+	mesh.visible = false
+	pop.play()
+	await get_tree().create_timer(1).timeout
+	target.queue_free()
