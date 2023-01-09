3c325c4505c4023125121eb4ee77bf4b0cad0a80
Stefano Pigozzi <me@steffo.eu>
2023-01-09 15:15:59 +0100

    Add name input on main menu

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -7,3 +7,7 @@ signal play_pressed()
 func play():
 	print("Play button was pressed.")
 	emit_signal("play_pressed")
+
+
+func _on_text_edit_text_changed():
+	Singletons.username = $Content/Inputs/NameInput.text
