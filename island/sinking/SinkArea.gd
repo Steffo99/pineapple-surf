86573b0a2cd269a1c55011c0ec6a9aa6a8b84507
Stefano Pigozzi <me@steffo.eu>
2023-01-09 12:39:20 +0100

    Move sinking-related scenes to `island/sinking`

--- island/sinking/SinkArea.gd
+++ island/sinking/SinkArea.gd
@@ -4,11 +4,11 @@ class_name SinkArea
 
 ## A node fell in the water.
 ## Triggers after CanSink.sunk .
-signal has_sunk(splasher: CanSink)
+signal has_sunk(sinkable: CanSink)
 
 
 func _on_body_entered(body: Node3D):
-	var sinkables: Array[CanSink] = body.find_children("*", "CanSink", false, false)
+	var sinkables = body.find_children("*", "CanSink", false, false)
 	for sinkable in sinkables:
 		sinkable.sink()
 		emit_signal("has_sunk", sinkable)
