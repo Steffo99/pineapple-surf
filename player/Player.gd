e3895e7d44d74a55fc631d417174d381d80d8e76
Stefano Pigozzi <me@steffo.eu>
2023-01-09 02:41:46 +0100

    Change seed collect sound

--- player/Player.gd
+++ player/Player.gd
@@ -130,6 +130,9 @@ func _process(_delta: float) -> void:
 
 
 func _input(event):
+	if event.is_action_pressed("respawn"):
+		respawn()
+
 	if event is InputEventKey and event.keycode == KEY_ESCAPE:
 		get_tree().quit()
 	
