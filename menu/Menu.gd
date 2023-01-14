29a901f3600d35fcbb32e9d854086a4e7bdb8ce4
Stefano Pigozzi <me@steffo.eu>
2023-01-14 22:49:58 +0100

    Fix username input

--- menu/Menu.gd
+++ menu/Menu.gd
@@ -3,14 +3,31 @@ extends Control
 signal play_pressed()
 
 
+var username_regex
+
+
+var username: String:
+	get:
+		return Singletons.username
+	set(value):
+		value = value.strip_escapes()
+		value = value.to_lower()
+		value = username_regex.sub(value, "-", true)
+		Singletons.username = value
+		var caret = $Content/Inputs/NameInput.caret_column
+		$Content/Inputs/NameInput.text = value
+		$Content/Inputs/NameInput.caret_column = caret
+		%PlayButton.disabled = len(value) <= 0
+
+
 func play():
 	print("Player ", Singletons.username, " started playing!")
 	emit_signal("play_pressed")
 
 
-func _on_name_input_text_changed():
-	Singletons.username = $Content/Inputs/NameInput.text
-	%PlayButton.disabled = false
+func _on_name_input_text_changed(new_text):
+	if username != $Content/Inputs/NameInput.text:
+		username = $Content/Inputs/NameInput.text
 
 
 func _on_fetched_scores():
@@ -19,5 +36,7 @@ func _on_fetched_scores():
 
 
 func _ready():
+	username_regex = RegEx.new()
+	username_regex.compile("[^a-z0-9]")
 	$"/root/BaseScene/ScoreboardContainer".connect("fetched_scores", _on_fetched_scores)
 	$"/root/BaseScene/ScoreboardContainer".fetch_scores(false)
